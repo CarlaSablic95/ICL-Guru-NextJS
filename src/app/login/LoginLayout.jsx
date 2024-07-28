07bf6869d1bdec216a47dad117f8aa447b6beac5
CarlaSablic95 <carlasablic95@gmail.com>
2024-07-28 19:14:34 -0300

    add(-): Creación de estilos css, y plantilla para el componente de Login

---
+++ src/app/login/LoginLayout.jsx
@@ -0,0 +1,9 @@
+const LoginLayout = ({children}) => {
+    return (
+        <section className="login-container">
+            {children}
+        </section>
+    );
+}
+
+export default LoginLayout;
