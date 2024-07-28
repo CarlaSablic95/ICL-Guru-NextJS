25c651a83be1152e2c2729ae77c6e0886eba98bb
CarlaSablic95 <carlasablic95@gmail.com>
2024-07-28 19:15:26 -0300

    add(-): Creación de Plantilla

---
+++ src/app/layout.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import metadata from "./metadata";
+import { usePathname } from "next/navigation";
+import dynamic from "next/dynamic";
+import { Providers } from "@/provider/Provider";
+import Header from "@/components/Header/Header";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import NavbarMobile from "@/components/NavbarMobile/NavbarMobile";
+import EnterPassword from "@/components/Modal/EnterPassword";
+import Logout from "@/components/Modal/Logout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+
+const ClientBootstrap = dynamic(() => import("@/components/ClientBootstrap/ClientBootstrap"), { ssr: false });
+
+console.log(metadata);
+
+export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/login";
+
+  return (
+    <html lang="en">
+      <Providers>
+        <body className="d-flex flex-column min-vh-100">
+          <ClientBootstrap />
+          {!isLoginPage && (
+            <>
+              <Header />
+              <div className="container-fluid">
+                <div className="row justify-content-evenly">
+                  <div className="d-none d-lg-block col-lg-2 px-0">
+                    <Sidebar />
+                  </div>
+                  <div className="d-block d-lg-none px-0">
+                    <NavbarMobile />
+                  </div>
+                  <main className="col-12 col-lg-10 px-0">
+                    {children}
+                  </main>
+                </div>
+              </div>
+            </>
+          )}
+          {isLoginPage && (
+            <main>
+              {children}
+            </main>
+          )}
+          <EnterPassword />
+          <Logout />
+        </body>
+      </Providers>
+    </html>
+  );
+}
